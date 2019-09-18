0a52a12a92d6f5e5a7a579fdaa569e683326794f
zhanglang <zhanglang1@dgg.net>
2019-09-18 17:32:24 +0800

    添加配置

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,6 +1,18 @@
 <template>
-	<view>
-		登录页面
+	<view class='zh_login'>
+		<view class='zh_login_Input'>
+			<view class='zh_imgTitle'>
+				<img src="../../static/loginImg/sjh.png" alt="">
+			</view>
+			<input type="text" placeholder="请输入账号">
+		</view>
+		<view class='zh_login_Input'>
+			<view class='zh_imgTitle'>
+				<img src="../../static/loginImg/yzm.png" alt="">
+			</view>
+			<input type="password" placeholder="请输入密码">
+		</view>
+		<view class='zh_login_Input blue' @click='subMit'>登录</view>
 	</view>
 </template>
 
@@ -15,5 +27,22 @@
 </script>
 
 <style lang="less">
-
+	.blue{
+		background-color: #2292DD !important;color:#fff !important;text-align: center;
+	}
+	.zh_login{
+		padding:20px;font-size:14px;
+		.zh_login_Input{
+				background-color:#f2f2f2;border-radius:5px;margin-bottom:25px;height:40px;overflow: hidden;position:relative;line-height:40px;
+				.zh_imgTitle{
+					position:absolute;width:17px;height:17px;left:15px;top:5px;
+					img{
+						width:100%;height:100%;
+					}
+				}
+				input{
+					display: block;width:100%;border:none;background-color:#f2f2f2;height:100%;text-indent:15px;padding-left:30px;
+				}
+		}
+	}
 </style>
